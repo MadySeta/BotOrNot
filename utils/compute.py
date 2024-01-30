883078527f5bb3b386b1fb519e63373cab6fe7bb
IncredibleYu <101401043+IncredibleYu@users.noreply.github.com>
2024-01-30 09:31:14 +0100

    Ajout humanratio

--- utils/compute.py
+++ utils/compute.py
@@ -3,7 +3,10 @@ import numpy as np
 def BotRatio(all_predictions : list):
   ratio = all_predictions.count("bot")/len(all_predictions)
   return ratio 
-
+def HumanRatio(all_predictions : list):
+  ratio = all_predictions.count("bot")/len(all_predictions)
+  return ratio 
+  
 def FollowersBotHumanPercentage( humanprobabilities, botprobabilities):
     trust_bot_array = np.array(botprobabilities)
     trust_human_array = np.array(humanprobabilities)
